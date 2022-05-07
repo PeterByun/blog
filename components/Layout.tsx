28cd76928cdf002f8d3025d9792685a45cb11c27
peter1035k <peter1035k@gmail.com>
2022-05-07 17:36:44 +0900

    feat: storage

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main>{children}</main>
+      <div className="nav-placeholder"></div>
       <Nav />
     </>
   );
