a099f56f4e8f928797777d47a9d8a7897248cf29
peter1035k <peter1035k@gmail.com>
2022-05-22 21:28:18 +0900

    feat: added title to profile

--- components-pages/profile/Introduction.tsx
+++ components-pages/profile/Introduction.tsx
@@ -2,15 +2,19 @@ import { H } from "../../components-base/H";
 
 export const Introduction = () => {
   return (
-    <H level={3}>
-      안녕하세요! <br />
-      <br />
-      안정적이고 편리한 서비스를 만들기위해 노력하는
-      <br />
-      <br />
-      프론트엔드 개발자 변동혁 입니다.
-      <br />
-      <br />
-    </H>
+    <>
+      <H level={1}>소개</H>
+
+      <H level={3}>
+        안녕하세요! <br />
+        <br />
+        안정적이고 편리한 서비스를 만들기위해 노력하는
+        <br />
+        <br />
+        프론트엔드 개발자 변동혁 입니다.
+        <br />
+        <br />
+      </H>
+    </>
   );
 };
