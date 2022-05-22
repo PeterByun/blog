2627c4850a5ae24feaa31cac18b1a209f68ba29f
peter1035k <peter1035k@gmail.com>
2022-05-22 21:57:04 +0900

    feat: added skill images

--- components-pages/profile/Skills.tsx
+++ components-pages/profile/Skills.tsx
@@ -1,42 +1,50 @@
 import { H } from "../../components-base/H";
 import { Container } from "../../components-base/Container";
 
+const SkillLogo = (props: { fileName: string }) => {
+  return (
+    <img
+      className="skill-logo"
+      src={`/assets/icons/skills/${props.fileName}`}
+      alt={props.fileName}
+      width="100"
+      height="100"
+    />
+  );
+};
+
 export const Skills = () => {
   return (
     <>
       <H level={1}>기술</H>
 
-      <Container className="container-2 margin-bottom-1">
-        <H level={2}>Front-end</H>
-        <ol>
-          <li>JavaScript</li>
-          <li>TypesScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Emotion</li>
-          <li>Webpack</li>
-          <li>Babel</li>
-          <li>ESLint</li>
-          <li>Vue</li>
-          <li>React</li>
-          <li>Vuex</li>
-          <li>Redux</li>
-          <li>Jest</li>
-          <li>Cypress</li>
-          <li>i18n</li>
-        </ol>
+      <H level={2}>Front-end</H>
+
+      <Container className="skills-container container-1 margin-bottom-1">
+        <SkillLogo fileName="babel.svg" />
+        <SkillLogo fileName="css-3.svg" />
+        <SkillLogo fileName="html-5.svg" />
+        <SkillLogo fileName="cypress.svg" />
+        <SkillLogo fileName="eslint.svg" />
+        <SkillLogo fileName="github.svg" />
+        <SkillLogo fileName="javascript.svg" />
+        <SkillLogo fileName="typescript.svg" />
+        <SkillLogo fileName="vue.svg" />
+        <SkillLogo fileName="react.svg" />
+        <SkillLogo fileName="webpack.svg" />
+        <SkillLogo fileName="jest.svg" />
+        <SkillLogo fileName="emotion.png" />
+        <SkillLogo fileName="vue-i18n.png" />
       </Container>
 
-      <Container className="container-2 margin-bottom-1">
-        <H level={2}>DevOps</H>
-        <ol>
-          <li>Linux</li>
-          <li>AWS S3</li>
-          <li>AWS CloudFront</li>
-          <li>AWS WAF</li>
-          <li>AWS EC2</li>
-          <li>Github Actions</li>
-        </ol>
+      <H level={2}>DevOps</H>
+      <Container className="skills-container container-1 margin-bottom-1">
+        <SkillLogo fileName="linux-tux.svg" />
+        <SkillLogo fileName="github-actions.svg" />
+        <SkillLogo fileName="aws-cloudfront.svg" />
+        <SkillLogo fileName="aws-ec2.svg" />
+        <SkillLogo fileName="aws-s3.svg" />
+        <SkillLogo fileName="aws-waf.svg" />
       </Container>
     </>
   );
